701f6327d7dae631e7d05dad3abfb61583c0c163
Chenath <chenathk@gmail.com>
2025-02-15 16:43:07 +0530

    added
    code in user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require('Mongoose');
 
 const UserSchema = mongoose.Schema({
-    
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    googleId: { type: String }, 
+    createdAt: { type: Date, default: Date.now }
 })
+console.log('Hey')
